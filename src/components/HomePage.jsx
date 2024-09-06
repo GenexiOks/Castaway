a588e1b2ff81dbb8d5d3f68f88ddff9966fec01d
Pavel <Pashaz1994@gmail.com>
2024-09-06 18:44:25 +0300

    Add font

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,19 @@
-import styles from './HomePage.module.css';
-import logo from '../../logo/Vector.svg';
+import styles from "./HomePage.module.css";
+import logo from "../../logo/Vector.svg";
 
 function HomePage() {
     return (
         <>
             <div className={styles.header}>
                 <section className={styles.section}>
-                    <img src={logo} className={styles.logo} alt={'logo'} />
-                    <nav className={styles['navigation-bar']}></nav>
+                    <img src={logo} className={styles.logo} alt={"logo"} />
                 </section>
+                <nav className={styles["navigation-bar"]}>
+                    <a href="form">Home</a>
+                    <a href="form">Episodes</a>
+                    <a href="#form">About</a>
+                    <a href="form">Contact</a>
+                </nav>
             </div>
         </>
     );

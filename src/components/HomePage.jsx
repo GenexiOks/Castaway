37dbcbbd7eb10e3d0490f22b0a8edd5c3e4d63b1
Genexi <Pashaz1994@gmail.com>
2024-09-06 08:10:16 +0300

    first commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,17 @@
-import styles from "./HomePage.module.css"
+import styles from './HomePage.module.css';
+import logo from '../../logo/Vector.svg';
 
-function HomePage(){
-    return <>
-        <div className={styles.header}>sdvfwd</div>
-    
-    </>
+function HomePage() {
+    return (
+        <>
+            <div className={styles.header}>
+                <section className={styles.section}>
+                    <img src={logo} className={styles.logo} alt={'logo'} />
+                    <nav className={styles['navigation-bar']}></nav>
+                </section>
+            </div>
+        </>
+    );
 }
 
-export default HomePage
+export default HomePage;

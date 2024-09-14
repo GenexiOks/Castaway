bbfe46dd09da8d1e831a72cedcbad19236e00f0b
Genexi <Pashaz1994@gmail.com>
2024-09-14 09:13:02 +0300

    second

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,8 @@
-import styles from "./HomePage.module.css";
-import logo from "../../logo/Vector.svg";
-import img from "../../img/IMAGE.svg";
-import lines from "../../img/IMAGE1.svg";
-import vector from "../../img/Vector.svg";
+import styles from './HomePage.module.css';
+import logo from '../../logo/Vector.svg';
+import img from '../../img/IMAGE.svg';
+import lines from '../../img/IMAGE1.svg';
+import vector from '../../img/Vector.svg';
 
 function HomePage() {
     return (
@@ -10,7 +10,7 @@ function HomePage() {
             <div className={styles.header}>
                 <nav>
                     <img src={logo}></img>
-                    <div className={styles["navigation-panel"]}>
+                    <div className={styles['navigation-panel']}>
                         <a href="#" className={styles.home}>
                             Home
                         </a>
@@ -27,58 +27,58 @@ function HomePage() {
                 </nav>
             </div>
 
-            <div className={styles["home-page"]}>
-                <section className={styles["section-one"]}>
-                    <div className={styles["content-one"]}>
+            <div className={styles['home-page']}>
+                <section className={styles['section-one']}>
+                    <div className={styles['content-one']}>
                         <img
                             src={lines}
                             alt="line"
-                            className={styles["img-2"]}
+                            className={styles['img-2']}
                         />
                         <img
                             src={img}
                             alt="home page"
-                            className={styles["img-1"]}
+                            className={styles['img-1']}
                         />
-                        <div className={styles["next-level-text"]}>
+                        <div className={styles['next-level-text']}>
                             <p className={styles.next}>
                                 next
                                 <img src={vector} />
                             </p>
-                            <p className={styles["take-your-podcast"]}>
+                            <p className={styles['take-your-podcast']}>
                                 Take your podcast to the
                             </p>
                             <p className={styles.level}>level</p>
                             <div
                                 className={
-                                    styles["conteiner-for-social-networking"]
+                                    styles['conteiner-for-social-networking']
                                 }
                             >
                                 <p>Listen on</p>
                                 <nav
-                                    className={styles["conteiner-social-logo"]}
+                                    className={styles['conteiner-social-logo']}
                                 >
                                     <div>
-                                        <img src={"./social_logo/IMAGE.png"} />
+                                        <img src={'./social_logo/IMAGE.png'} />
                                     </div>
                                     <div>
                                         <img
-                                            src={"./social_logo/IMAGE (4).png"}
+                                            src={'./social_logo/IMAGE (4).png'}
                                         />
                                     </div>
                                     <div>
                                         <img
-                                            src={"./social_logo/IMAGE (3).png"}
+                                            src={'./social_logo/IMAGE (3).png'}
                                         />
                                     </div>
                                     <div>
                                         <img
-                                            src={"./social_logo/IMAGE (2).png"}
+                                            src={'./social_logo/IMAGE (2).png'}
                                         />
                                     </div>
                                     <div>
                                         <img
-                                            src={"./social_logo/IMAGE (1).png"}
+                                            src={'./social_logo/IMAGE (1).png'}
                                         />
                                     </div>
                                 </nav>
@@ -87,26 +87,41 @@ function HomePage() {
                     </div>
                 </section>
             </div>
-            <div className={styles["conteiner-about-us"]}>
+            <div className={styles['conteiner-about-us']}>
                 <section>
-                    <div className={styles["all-episodes-text"]}>
+                    <div className={styles['all-episodes-text']}>
                         <p>Latest episodes</p>
                         <button>View all episodes</button>
                     </div>
-                    <div className={styles["all-episodes-preview"]}>
-                        <div className={styles["episode-one"]}>
+                    <div className={styles['all-episodes-preview']}>
+                        <div className={styles['episode-one']}>
                             <section>
                                 <img src="./episodes_img/3.svg" />
+                                <div className={styles['episode-text']}>
+                                    <p className={styles['header-text']}>
+                                        Should you get outboard audio gear?
+                                    </p>
+                                </div>
                             </section>
                         </div>
-                        <div className={styles["episode-second"]}>
+                        <div className={styles['episode-second']}>
                             <section>
                                 <img src="./episodes_img/1.svg" />
+                                <div className={styles['episode-text']}>
+                                    <p className={styles['header-text']}>
+                                        Mic tricks to take you to the next level
+                                    </p>
+                                </div>
                             </section>
                         </div>
-                        <div className={styles["episode-third"]}>
+                        <div className={styles['episode-third']}>
                             <section>
                                 <img src="./episodes_img/2.svg" />
+                                <div className={styles['episode-text']}>
+                                    <p className={styles['header-text']}>
+                                        The best microphone under $200
+                                    </p>
+                                </div>
                             </section>
                         </div>
                     </div>
